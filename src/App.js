1e5054143a9e9a66b030803c4111a1ab16382047
unknown <theitalianpeter@hotmail.com>
2019-04-25 19:02:36 +0100

    app commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-
-import './App.css';
+import TodoInput from  './components/TodoInput'
+import TodoList from  './components/TodoList'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import uuid from 'uuid';
 
 function App() {
   return (
-    <div >
-      
+    <div class="container">
+      <div class="row">
+        <div class="col-10 mx-auto col-md-8 mt-4">
+        <h3 className="text-capitalize text-center">Todo Input</h3>
+        <TodoInput/>
+        <TodoList/>
+        </div>
+      </div>
     </div>
   );
 }
