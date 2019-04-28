15213dd5411a6f96b9bcd071149d487612a8b2b2
unknown <theitalianpeter@hotmail.com>
2019-04-28 23:52:33 +0100

    small update

--- src/App.js
+++ src/App.js
@@ -5,13 +5,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import uuid from 'uuid';
 
 function App() {
+
+  state={
+    items: [],
+    id: 0,
+    item= "",
+    editItem: false
+  };
+  handleChange = e => {
+    this.setState({
+        item: e.target.value
+    });
+
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-10 mx-auto col-md-8 mt-4">
         <h3 className="text-capitalize text-center">Todo Input</h3>
-        <TodoInput/>
-        <TodoList/>
+          <TodoInput item={this.state.item} 
+          handleChange={this.handleChange}/>
+          <TodoList/>
         </div>
       </div>
     </div>
